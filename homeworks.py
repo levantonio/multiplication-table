ac68eae7f260c663b8af965e4cfa5795bc32fcc6
Anton <levantonio@mail.ru>
2022-11-25 07:41:15 +0300

    homeworks

---
+++ homeworks.py
@@ -0,0 +1,79 @@
+# print('Введите число 1')
+# a = int(input())
+# print('Введите число 2')
+# b = int(input())
+# if a > b:
+#     print(f"max из {a} и {b}- > {a}")
+# else:
+#     print(f"max из {a} и {b}- > {b}")
+
+
+# print('Введите число 1')
+# a = int(input())
+# print('Введите число 2')
+# b = int(input())
+# print('Введите число 3')
+# c = int(input())
+# if a > b or a > c:
+#     print(f"max из {a} и {b} и {c} -> {a}")
+# elif b > a or b > c:
+#     print(f"max из {a} и {b} и {c} -> {b}")
+# else:
+#     print(f"max из {a} и {b} и {c} -> {c}")
+
+
+# print('Введите число ')
+# a = int(input())
+# if a % 2 == 0:
+#     print(f"{a} -> да")
+# else:
+#     print(f"{a} -> нет")
+
+
+# print('Введите число ')
+# a = int(input())
+
+# for i in range(a):
+#     if i % 2 == 0:
+#         print(i)
+
+
+# print('Введите число ')
+# a = int(input())
+# a = (a // 10) % 10
+# print(a)
+
+
+# print('Введите число ')
+# a = int(input())
+# if 99 < a < 1000:
+#     print(a % 10)
+# elif a >= 1000:
+#     while a >= 1000:
+#         a = a // 10
+#     print(a % 10)
+# else:
+#     print(f"Число {a} двузначное")
+
+
+# print('Введите число ')
+# a = int(input())
+# if a == 1 or a == 2 or a == 3 or a == 4 or a == 5:
+#     print('будни')
+# elif a == 6 or a == 7:
+#     print('Выходной')
+# else:
+#     print('это не день недели')
+
+
+
+
+print('Введите число ')
+number = int(input())
+i = 1
+while i <= number:
+    print(f"{i} in cube = {i ** 1583}")
+    i += 1
+        
+
+
